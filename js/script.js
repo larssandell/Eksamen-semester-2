8504d27d47dc3097efb963ea99f0bc2916bb51e8
larssandell <91052943+larssandell@users.noreply.github.com>
2022-05-01 14:49:12 +0200

    fortsatt på footer header
    Og lagt inn innhold i main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+const apiUrl = "https://si.larssandell.no/wp-json/wp/v2/posts/";
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
 
+//API Call
+async function callApi() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+
+
+    }catch(err) {
+        console.log("Her er feilen " ,err);
+    }
+}
+callApi();
+
+
+// hamburger menu
 hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
